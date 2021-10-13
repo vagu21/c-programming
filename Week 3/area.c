f8eb70e2649cd28291010d6803d6d22b99185535
vagu21 <bharadwaj21197@gmail.com>
2021-10-13 21:57:11 +0530

    i added area

---
+++ Week 3/area.c
@@ -0,0 +1,20 @@
+/******************************************************************************
+
+Welcome to GDB Online.
+GDB online is an online compiler and debugger tool for C, C++, Python, Java, PHP, Ruby, Perl,
+C#, VB, Swift, Pascal, Fortran, Haskell, Objective-C, Assembly, HTML, CSS, JS, SQLite, Prolog.
+Code, Compile, Run and Debug online from anywhere in world.
+
+*******************************************************************************/
+#include <stdio.h>
+
+int main()
+{
+    float pi=3.14,r,area;
+    printf("what is the value of r\n");
+    scanf("%f",&r);
+    area=pi*r*r;
+printf("%f",area);
+    return 0;
+}
+
