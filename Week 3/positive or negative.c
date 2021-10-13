9b28f287c71218cc12d9e05278369fa32e238ac7
vagu21 <bharadwaj21197@gmail.com>
2021-10-13 22:30:44 +0530

    positive or negative

---
+++ Week 3/positive or negative.c
@@ -0,0 +1,24 @@
+/******************************************************************************
+
+Welcome to GDB Online.
+GDB online is an online compiler and debugger tool for C, C++, Python, PHP, Ruby, 
+C#, VB, Perl, Swift, Prolog, Javascript, Pascal, HTML, CSS, JS
+Code, Compile, Run and Debug online from anywhere in world.
+
+*******************************************************************************/
+/* the simple if */
+#include <stdio.h>
+
+int main()
+{
+    int num;
+    printf("enter the number\n");
+    scanf("%d",&num);
+    if(num>0)
+          printf("%d is the positive number",num);
+          
+    
+
+    return 0;
+}
+
